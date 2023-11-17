d0d5345854dc2add03fb64dbf947e0f647a2ec7c
Eduardo <eduardo.mfigueredo@hotmail.com>
2023-11-17 15:17:36 -0300

    alteracoes

--- modulo11/atividade-pratica-gabarito/ListarTarefa.jsx
+++ modulo11/atividade-pratica-gabarito/ListarTarefa.jsx
@@ -24,13 +24,12 @@ function createData(
 }
 
 //Definição do array contendo os dados iniciais da listagem de tarefas
+
+// realizadas alterações
 const initialRows = [
-  createData(1, 'Tarefa 1', 'Descrição da Tarefa 1', '2022-01-01', '2022-01-02', 'Concluída', 'Recurso 1'),
-  createData(2, 'Tarefa 2', 'Descrição da Tarefa 2', '2022-01-03', '2022-01-04', 'Em Andamento', 'Recurso 2'),
-  createData(3, 'Tarefa 3', 'Descrição da Tarefa 3', '2022-01-04', '2022-01-05', 'Em Andamento', 'Recurso 3'),
-  createData(4, 'Tarefa 4', 'Descrição da Tarefa 4', '2022-01-05', '2022-01-06', 'Em Andamento', 'Recurso 4'),
-  createData(5, 'Tarefa 5', 'Descrição da Tarefa 5', '2022-01-06', '2022-01-07', 'Em Andamento', 'Recurso 5'),
-  createData(6, 'Tarefa 6', 'Descrição da Tarefa 6', '2022-01-07', '2022-01-08', 'Aguardando', 'Recurso 6'),
+  createData(1, 'Tarefa 1', 'Modificar o código, seja alterando a parte de estilos, seja modificando algum comportamento;', '2023-11-17', '2023-11-17', 'Concluída', 'Recurso 1'),
+  createData(2, 'Tarefa 2', 'Criar um repositório no seu Git pessoal;', '2023-11-17', '2023-11-17', 'Concluída', 'Recurso 2'),
+  createData(3, 'Tarefa 3', 'Fazer o commit das alterações e subir o projeto, modificado, para o seu Git pessoal.', '2022-01-04', '2022-01-05', 'Concluída', 'Recurso 3')
 ];
 
 //Componente ListarTarefa
